2f50b4f2f91b7cf4ab30e0128f1612b7db4e4436
username115 <username115@users.noreply.github.com>
2015-02-14 13:51:16 -0500

    removing call to new API until the API reading is fixed

--- src/org/growingstems/scouting/MatchSchedule.java
+++ src/org/growingstems/scouting/MatchSchedule.java
@@ -30,7 +30,7 @@ import android.widget.Toast;
 public class MatchSchedule implements HttpCallback {
 
 	private static final String FILENAME = "FRCscoutingschedule";
-	private static final String FRC_API_URL = "http://private-anon-4aa1e3079-frcevents.apiary-mock.com"; //using mock server for now
+	private static final String FRC_API_URL = "";//"http://private-anon-4aa1e3079-frcevents.apiary-mock.com"; //using mock server for now
 	private static final String API_CALL = "/schedule/2015/";
 
 	private boolean offseason = false;
