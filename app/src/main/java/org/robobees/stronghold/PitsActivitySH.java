0018f6067d628db8c53f60e3e820421c38c7b5e3
username115 <username115@users.noreply.github.com>
2016-01-27 13:12:32 -0500

    Designed Pit Layout and created empty activity for it

---
+++ app/src/main/java/org/robobees/stronghold/PitsActivitySH.java
@@ -0,0 +1,15 @@
+package org.robobees.stronghold;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+
+import org.growingstems.scouting.R;
+
+public class PitsActivitySH extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.pits);
+    }
+}
