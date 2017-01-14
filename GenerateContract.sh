c1f0a64aea17b30f0b83f75b99741330103a39fb
username115 <username115@users.noreply.github.com>
2017-01-13 19:52:13 -0500

    Updated organization of python generation script in preparation for more generated code

--- GenerateContract.sh
+++ GenerateContract.sh
@@ -1,3 +1,3 @@
 #! /bin/bash
 
-python SQLITEContractGen.py --packagename=org.frc836.database --classname=FRCScoutingContract --infile=FRC_Scouting_Server/scouting.sql --outfile=app/src/main/java/org/frc836/database/FRCScoutingContract.java
+python3 gen_scripts/SQLITEContractGen.py --packagename=org.frc836.database --classname=FRCScoutingContract --infile=FRC_Scouting_Server/scouting.sql --outfile=app/src/main/java/org/frc836/database/FRCScoutingContract.java
