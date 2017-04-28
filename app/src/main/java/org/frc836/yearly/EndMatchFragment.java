a1afb49f29900ad5c341973f66a7689df07a8a37
username115 <username115@users.noreply.github.com>
2017-04-28 10:47:41 -0400

    Text entered for climb or align times clears the Unknown checkbox. Checkbox is checked if text is cleared. Fixes #185

--- app/src/main/java/org/frc836/yearly/EndMatchFragment.java
+++ app/src/main/java/org/frc836/yearly/EndMatchFragment.java
@@ -17,6 +17,8 @@ package org.frc836.yearly;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -79,6 +81,10 @@ public class EndMatchFragment extends MatchFragment {
         pastNotes = (Spinner)getView().findViewById(R.id.previousNotes);
         commonNotes.setOnItemSelectedListener(new NotesSelectedListener());
         pastNotes.setOnItemSelectedListener(new NotesSelectedListener());
+        ((EditText)getView().findViewById(R.id.alignTimeT)).addTextChangedListener(
+                new TimeTextChangedListener((CheckBox)getView().findViewById(R.id.alignTimeUnknownC)));
+        ((EditText)getView().findViewById(R.id.climbTimeT)).addTextChangedListener(
+                new TimeTextChangedListener((CheckBox)getView().findViewById(R.id.climbTimeUnknownC)));
     }
 
     public void onResume() {
@@ -175,6 +181,73 @@ public class EndMatchFragment extends MatchFragment {
         public void onNothingSelected(AdapterView<?> parent) {
         }
 
-    };
+    }
+
+    class TimeTextChangedListener implements TextWatcher {
+
+        private CheckBox m_unknown;
+
+        public TimeTextChangedListener(CheckBox unknownC) {
+            m_unknown = unknownC;
+        }
+
+        /**
+         * This method is called to notify you that, within <code>s</code>,
+         * the <code>count</code> characters beginning at <code>start</code>
+         * are about to be replaced by new text with length <code>after</code>.
+         * It is an error to attempt to make changes to <code>s</code> from
+         * this callback.
+         *
+         * @param s
+         * @param start
+         * @param count
+         * @param after
+         */
+        @Override
+        public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            // Auto-generated method stub
+        }
+
+        /**
+         * This method is called to notify you that, within <code>s</code>,
+         * the <code>count</code> characters beginning at <code>start</code>
+         * have just replaced old text that had length <code>before</code>.
+         * It is an error to attempt to make changes to <code>s</code> from
+         * this callback.
+         *
+         * @param s
+         * @param start
+         * @param before
+         * @param count
+         */
+        @Override
+        public void onTextChanged(CharSequence s, int start, int before, int count) {
+            // Auto-generated method stub
+        }
+
+        /**
+         * This method is called to notify you that, somewhere within
+         * <code>s</code>, the text has been changed.
+         * It is legitimate to make further changes to <code>s</code> from
+         * this callback, but be careful not to get yourself into an infinite
+         * loop, because any changes you make will cause this method to be
+         * called again recursively.
+         * (You are not told where the change took place because other
+         * afterTextChanged() methods may already have made other changes
+         * and invalidated the offsets.  But if you need to know here,
+         * you can use {@link Spannable#setSpan} in {@link #onTextChanged}
+         * to mark your place and then look up from here where the span
+         * ended up.
+         *
+         * @param s
+         */
+        @Override
+        public void afterTextChanged(Editable s) {
+            if (s.length() > 0)
+                m_unknown.setChecked(false);
+            else
+                m_unknown.setChecked(true);
+        }
+    }
 
 }
