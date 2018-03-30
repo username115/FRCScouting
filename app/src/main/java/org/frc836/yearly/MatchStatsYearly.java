3eaa62de39f687abd8a7cfc41d679269f10213fb
username115 <username115@users.noreply.github.com>
2018-03-30 09:50:23 -0400

    Added even more graphs

--- app/src/main/java/org/frc836/yearly/MatchStatsYearly.java
+++ app/src/main/java/org/frc836/yearly/MatchStatsYearly.java
@@ -8,10 +8,13 @@ import java.util.List;
 
 public abstract class MatchStatsYearly {
 
-    public static final int NUM_GRAPHS = 3;
+    public static final int NUM_GRAPHS = 5;
     public static final int TOTAL_CUBES = 0;
-    public static final int EXCHANGE_CUBES = 1;
-    public static final int PARK_CLIMB_POINTS = 2;
+    public static final int AUTO_CUBES = 1;
+    public static final int SCALE_ONLY = 2;
+    public static final int EXCHANGE_CUBES = 3;
+    public static final int PARK_CLIMB_POINTS = 4;
+
 
     public static int getTotalScore(final MatchStatsStruct stats) {
         return 0; //no way to calculate score from stats this year, time-based
@@ -19,7 +22,9 @@ public abstract class MatchStatsYearly {
 
     public static List<String> getGraphNames() {
         List<String> ret = new ArrayList<String>(NUM_GRAPHS);
-        ret.add(TOTAL_CUBES,"Total Cubes Scored");
+        ret.add(TOTAL_CUBES, "Total Cubes Scored");
+        ret.add(AUTO_CUBES, "Auto Cubes");
+        ret.add(SCALE_ONLY, "Cubes on Scale");
         ret.add(EXCHANGE_CUBES, "Cubes in Exchange");
         ret.add(PARK_CLIMB_POINTS, "Park/Climb Points");
 
@@ -30,6 +35,10 @@ public abstract class MatchStatsYearly {
         switch (statNum) {
             case TOTAL_CUBES:
                 return getTotalCubesScored(stats);
+            case AUTO_CUBES:
+                return getAutoCubes(stats);
+            case SCALE_ONLY:
+                return getScaleCubes(stats);
             case EXCHANGE_CUBES:
                 return getExchangeCount(stats);
             case PARK_CLIMB_POINTS:
@@ -41,6 +50,14 @@ public abstract class MatchStatsYearly {
     public static int getTotalCubesScored(final MatchStatsStruct stats) {
         return stats.auto_switch_count + stats.auto_scale_count + stats.switch_count
                 + stats.scale_count + stats.opposite_switch_count;
+    }
+
+    public static int getAutoCubes(final MatchStatsStruct stats) {
+        return stats.auto_switch_count + stats.auto_scale_count;
+    }
+
+    public static int getScaleCubes(final MatchStatsStruct stats) {
+        return stats.auto_scale_count + stats.scale_count;
     }
 
     public static int getExchangeCount(final MatchStatsStruct stats) {
