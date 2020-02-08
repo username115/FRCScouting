5dd2619e14a63ae42e160a143047a417ecffd0fe
username115 <username115@users.noreply.github.com>
2020-02-08 14:44:17 -0500

    Fix DB utility functions and csv export for updated table layout

--- app/src/main/java/org/frc836/database/ScoutingDBHelper.java
+++ app/src/main/java/org/frc836/database/ScoutingDBHelper.java
@@ -12,11 +12,6 @@ public class ScoutingDBHelper extends SQLiteOpenHelper {
     private static ScoutingDBHelper helper;
     public static final Object lock = new Object();
 
-    /*private static final String upgrade20174to20175[] = {"ALTER TABLE scout_pit_data_2017 ADD COLUMN robot_gross_weight_lbs unsigned int(4) NOT NULL DEFAULT 0;"};
-    private static final String upgrade20175to20176[] = {"ALTER TABLE fact_pilot_data_2017 ADD COLUMN gears_dropped unsigned int(3) NOT NULL DEFAULT 0;"};
-    private static final String upgrade20176to20177[] = {"ALTER TABLE fact_match_data_2017 ADD COLUMN climb_time int(3) NOT NULL DEFAULT -1;",
-                                                         "ALTER TABLE fact_match_data_2017 ADD COLUMN align_time int(3) NOT NULL DEFAULT -1;"};
-*/
     public ScoutingDBHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
     }
@@ -30,29 +25,9 @@ public class ScoutingDBHelper extends SQLiteOpenHelper {
 
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
-        //if (oldVersion < 20174 || oldVersion > newVersion) {
-            for (int i = 0; i < FRCScoutingContract.SQL_DELETE_ENTRIES.length; i++)
-                db.execSQL(FRCScoutingContract.SQL_DELETE_ENTRIES[i]);
-            onCreate(db);
-        /*} else {
-            switch (oldVersion) {
-                case 20174:
-                    for (String s : upgrade20174to20175) {
-                        db.execSQL(s);
-                    }
-                case 20175:
-                    for (String s : upgrade20175to20176) {
-                        db.execSQL(s);
-                    }
-                case 20176:
-                    for (String s : upgrade20176to20177) {
-                        db.execSQL(s);
-                    }
-                default:
-                    break;
-            }
-            DBActivity.dbUpdated();
-        }*/
+		for (int i = 0; i < FRCScoutingContract.SQL_DELETE_ENTRIES.length; i++)
+			db.execSQL(FRCScoutingContract.SQL_DELETE_ENTRIES[i]);
+		onCreate(db);
     }
 
     public void onDowngrade(SQLiteDatabase db, int oldVersion, int newVersion) {
