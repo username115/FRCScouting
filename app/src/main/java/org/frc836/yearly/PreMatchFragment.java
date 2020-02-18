1c02c0736d7cce42b83841223cc639cde2273bc6
username115 <username115@users.noreply.github.com>
2020-02-17 19:39:11 -0500

    Updated pre-match data entry

--- app/src/main/java/org/frc836/yearly/PreMatchFragment.java
+++ app/src/main/java/org/frc836/yearly/PreMatchFragment.java
@@ -22,6 +22,7 @@ import android.support.v4.content.ContextCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 import android.widget.FrameLayout;
 import android.widget.ImageButton;
 
@@ -34,254 +35,241 @@ import org.growingstems.scouting.R;
 public class PreMatchFragment extends MatchFragment {
 
 
-    private boolean displayed = false;
+	private boolean displayed = false;
 
-    private MatchStatsStruct tempData = new MatchStatsStruct();
+	private MatchStatsStruct tempData = new MatchStatsStruct();
 
-    //private Button swap_sides;
+	//these are in order, starting with player station number 1
+	//not the order that they appear in the layout
+	//getGUIRefs performs this mapping
+	private ImageButton pos1B;
+	private ImageButton pos2B;
+	private ImageButton pos3B;
+	private ImageButton pos4B;
+	private ImageButton pos5B;
 
-    private ImageButton hab1;
-    private ImageButton hab2L;
-    private ImageButton hab2R;
-    private ImageButton hab3;
-    private ImageButton cargo_preload;
-    private ImageButton hatch_preload;
-
-    private FrameLayout Lhab1;
-    private FrameLayout Lhab2L;
-    private FrameLayout Lhab2R;
-    private FrameLayout Lhab3;
-    private FrameLayout Lcargo_preload;
-    private FrameLayout Lhatch_preload;
-
-
-    private View mainView;
-
-    public PreMatchFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @return A new instance of fragment PreMatch.
-     */
-    public static PreMatchFragment newInstance() {
-        return new PreMatchFragment();
-    }
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
-
-    @Override
-    public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                             Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_pre_match, container, false);
-    }
-
-    @Override
-    public void onViewCreated(View view, Bundle savedInstanceState) {
-        mainView = view;
-        getGUIRefs(view);
-        //if (!Prefs.getRedLeft(getActivity(), true)) {
-        //    tempData.scale_right = true;
-        //    tempData.near_switch_right = true;
-        //    tempData.far_switch_right = true;
-        //}
-        setListeners();
-        displayed = true;
-    }
-
-    @Override
-    public void onResume() {
-        super.onResume();
-        loadData(tempData);
-    }
-
-    public void onPause() {
-        super.onPause();
-        saveData(tempData);
-    }
-
-    @Override
-    public void saveData(MatchStatsStruct data) {
-        if (getView() == null || data == null || !displayed)
-            return;
-
-        data.prematch_hab_level = tempData.prematch_hab_level;
-        data.prematch_robot_cargo = tempData.prematch_robot_cargo;
-        data.prematch_robot_hatch = tempData.prematch_robot_hatch;
-
-    }
-
-    @Override
-    public void loadData(MatchStatsStruct data) {
-        tempData = data;
-        if (getView() == null || data == null || !displayed)
-            return;
-        // which side are we using
-        //boolean redLeft = Prefs.getRedLeft(getActivity(), true);
-
-        //if (!redLeft)
-        //    scale.setScaleX(-1f);
-        //else
-        //    scale.setScaleX(1f);
+	private FrameLayout pos1L;
+	private FrameLayout pos2L;
+	private FrameLayout pos3L;
+	private FrameLayout pos4L;
+	private FrameLayout pos5L;
 
 
-        Activity act = getActivity();
-        String pos;
-        if (act instanceof MatchActivity)
-            pos = ((MatchActivity) act).getPosition();
-        else
-            pos = Prefs.getPosition(getActivity(), "Red 1");
+	private View mainView;
 
-        boolean blue = pos.contains("Blue");
+	public PreMatchFragment() {
+		// Required empty public constructor
+	}
 
-        //set colors based on side;
-        hab1.setImageResource(blue ? R.drawable.blue_hab_1 : R.drawable.red_hab_1);
-        hab2L.setImageResource(blue ? R.drawable.blue_hab_2_left : R.drawable.red_hab_2_left);
-        hab2R.setImageResource(blue ? R.drawable.blue_hab_2_right : R.drawable.red_hab_2_right);
-        hab3.setImageResource(blue ? R.drawable.blue_hab_3 : R.drawable.red_hab_3);
+	/**
+	 * Use this factory method to create a new instance of
+	 * this fragment using the provided parameters.
+	 *
+	 * @return A new instance of fragment PreMatch.
+	 */
+	public static PreMatchFragment newInstance() {
+		return new PreMatchFragment();
+	}
+
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+	}
+
+	@Override
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+							 Bundle savedInstanceState) {
+		// Inflate the layout for this fragment
+		return inflater.inflate(R.layout.fragment_pre_match, container, false);
+	}
+
+	@Override
+	public void onViewCreated(View view, Bundle savedInstanceState) {
+		mainView = view;
+		getGUIRefs(view);
+		setListeners();
+		Button swap_sides = view.findViewById(R.id.switchSidesB);
+		swap_sides.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				switchSides();
+			}
+		});
+		displayed = true;
+	}
+
+	@Override
+	public void onResume() {
+		super.onResume();
+		//need to remap view every time it is loaded, in case position or side changed
+		getGUIRefs(mainView);
+		setListeners();
+		loadData(tempData);
+	}
+
+	public void onPause() {
+		super.onPause();
+		saveData(tempData);
+	}
+
+	@Override
+	public void saveData(MatchStatsStruct data) {
+		if (getView() == null || data == null || !displayed)
+			return;
+		data.start_position = tempData.start_position;
+	}
+
+	@Override
+	public void loadData(MatchStatsStruct data) {
+		tempData = data;
+		if (getView() == null || data == null || !displayed)
+			return;
+		// which side are we using
+		boolean redLeft = Prefs.getRedLeft(getActivity(), true);
+
+		Activity act = getActivity();
+		String pos;
+		if (act instanceof MatchActivity)
+			pos = ((MatchActivity) act).getPosition();
+		else
+			pos = Prefs.getPosition(getActivity(), "Red 1");
+
+		boolean blue = pos.contains("Blue");
+
+		pos1B.setImageResource(blue ? R.drawable.blue_station_1 : R.drawable.red_station_1);
+		pos2B.setImageResource(blue ? R.drawable.blue_station_port : R.drawable.red_station_port);
+		pos3B.setImageResource(blue ? R.drawable.blue_station_2 : R.drawable.red_station_2);
+		pos4B.setImageResource(blue ? R.drawable.blue_station_bay : R.drawable.red_station_bay);
+		pos5B.setImageResource(blue ? R.drawable.blue_station_3 : R.drawable.red_station_3);
+
+		//if red is on left, flip all resources.
+		pos1B.setScaleY(redLeft ? -1f : 1f);
+		pos2B.setScaleY(redLeft ? -1f : 1f);
+		pos3B.setScaleY(redLeft ? -1f : 1f);
+		pos4B.setScaleY(redLeft ? -1f : 1f);
+		pos5B.setScaleY(redLeft ? -1f : 1f);
+
+		Drawable blackBorder = ContextCompat.getDrawable(mainView.getContext(), R.drawable.blackborder);
+		Drawable selectBorder = ContextCompat.getDrawable(mainView.getContext(), R.drawable.greenborder);
+		//set current selections from load
+		pos1L.setForeground(blackBorder);
+		pos2L.setForeground(blackBorder);
+		pos3L.setForeground(blackBorder);
+		pos4L.setForeground(blackBorder);
+		pos5L.setForeground(blackBorder);
+		switch (data.start_position) {
+			case 1:
+				pos1L.setForeground(selectBorder);
+				break;
+			case 2:
+				pos2L.setForeground(selectBorder);
+				break;
+			case 3:
+				pos3L.setForeground(selectBorder);
+				break;
+			case 4:
+				pos4L.setForeground(selectBorder);
+				break;
+			case 5:
+				pos5L.setForeground(selectBorder);
+				break;
+			default:
+				break;
+		}
+
+	}
+
+	private void getGUIRefs(View view) {
+		// which side are we using
+		boolean redLeft = Prefs.getRedLeft(getActivity(), true);
+		//and which position are we watching
+		Activity act = getActivity();
+		String pos;
+		if (act instanceof MatchActivity)
+			pos = ((MatchActivity) act).getPosition();
+		else
+			pos = Prefs.getPosition(getActivity(), "Red 1");
+
+		boolean blue = pos.contains("Blue");
+
+		if (blue ^ redLeft) {
+			//pos 1 is top
+			pos1L = view.findViewById(R.id.pos1L);
+			pos1B = view.findViewById(R.id.pos1B);
+			pos2L = view.findViewById(R.id.pos2L);
+			pos2B = view.findViewById(R.id.pos2B);
+			pos3L = view.findViewById(R.id.pos3L);
+			pos3B = view.findViewById(R.id.pos3B);
+			pos4L = view.findViewById(R.id.pos4L);
+			pos4B = view.findViewById(R.id.pos4B);
+			pos5L = view.findViewById(R.id.pos5L);
+			pos5B = view.findViewById(R.id.pos5B);
+
+		} else {
+			//pos 1 is bottom
+			pos1L = view.findViewById(R.id.pos5L);
+			pos1B = view.findViewById(R.id.pos5B);
+			pos2L = view.findViewById(R.id.pos4L);
+			pos2B = view.findViewById(R.id.pos4B);
+			pos3L = view.findViewById(R.id.pos3L);
+			pos3B = view.findViewById(R.id.pos3B);
+			pos4L = view.findViewById(R.id.pos2L);
+			pos4B = view.findViewById(R.id.pos2B);
+			pos5L = view.findViewById(R.id.pos1L);
+			pos5B = view.findViewById(R.id.pos1B);
+
+		}
+	}
+
+	private void setListeners() {
+		pos1B.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				toggleStartPos(1);
+			}
+		});
+		pos2B.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				toggleStartPos(2);
+			}
+		});
+		pos3B.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				toggleStartPos(3);
+			}
+		});
+		pos4B.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				toggleStartPos(4);
+			}
+		});
+		pos5B.setOnClickListener(new View.OnClickListener() {
+			@Override
+			public void onClick(View v) {
+				toggleStartPos(5);
+			}
+		});
+	}
+
+	private void toggleStartPos(int level) {
+		if (tempData.start_position == level)
+			tempData.start_position = 0;
+		else
+			tempData.start_position = level;
+		loadData(tempData); //apply to UI
+	}
 
 
-        Drawable blackBorder = ContextCompat.getDrawable(mainView.getContext(), R.drawable.blackborder);
-        Drawable selectBorder = ContextCompat.getDrawable(mainView.getContext(), R.drawable.greenborder);
-        //set current selections from load
-        Lhab1.setForeground(blackBorder);
-        Lhab2L.setForeground(blackBorder);
-        Lhab2R.setForeground(blackBorder);
-        Lhab3.setForeground(blackBorder);
-        switch (data.prematch_hab_level) {
-            case 1:
-                Lhab1.setForeground(selectBorder);
-                break;
-            case 2:
-                if (data.prematch_hab2_left)
-                    Lhab2L.setForeground(selectBorder);
-                else
-                    Lhab2R.setForeground(selectBorder);
-                break;
-            case 3:
-                Lhab3.setForeground(selectBorder);
-                break;
-            default:
-                break;
-        }
-
-        Lcargo_preload.setForeground(data.prematch_robot_cargo ? selectBorder : blackBorder);
-        Lhatch_preload.setForeground(data.prematch_robot_hatch ? selectBorder : blackBorder);
-
-        //scale.setScaleY((data.scale_right == redLeft) ? -1f : 1f);
-
-        //if (pos.contains("Blue") != redLeft) {
-        //    left_switch.setScaleY((data.near_switch_right == redLeft) ? -1f : 1f);
-        //    right_switch.setScaleY((data.far_switch_right == redLeft) ? -1f : 1f);
-        //} else {
-        //    left_switch.setScaleY((data.far_switch_right == redLeft) ? -1f : 1f);
-        //    right_switch.setScaleY((data.near_switch_right == redLeft) ? -1f : 1f);
-        //}
-
-
-    }
-
-    private void getGUIRefs(View view) {
-        hab1 = view.findViewById(R.id.hab1);
-        hab2L = view.findViewById(R.id.hab2Left);
-        hab2R = view.findViewById(R.id.hab2Right);
-        hab3 = view.findViewById(R.id.hab3);
-        cargo_preload = view.findViewById(R.id.cargoB);
-        hatch_preload = view.findViewById(R.id.hatchB);
-
-        Lhab1 = view.findViewById(R.id.hab1L);
-        Lhab2L = view.findViewById(R.id.hab2LeftL);
-        Lhab2R = view.findViewById(R.id.hab2RightL);
-        Lhab3 = view.findViewById(R.id.hab3L);
-        Lcargo_preload = view.findViewById(R.id.cargoL);
-        Lhatch_preload = view.findViewById(R.id.hatchL);
-    }
-
-    private void setListeners() {
-        //swap_sides.setOnClickListener(new View.OnClickListener() {
-        //    @Override
-        //    public void onClick(View v) {
-        //        switchSides();
-        //    }
-        //});
-        hab1.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleHabLevel(1, false);
-            }
-        });
-        hab2R.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleHabLevel(2, false);
-            }
-        });
-        hab2L.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleHabLevel(2, true);
-            }
-        });
-        hab3.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleHabLevel(3, false);
-            }
-        });
-
-
-        cargo_preload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleCargoHatch(false);
-            }
-        });
-        hatch_preload.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                toggleCargoHatch(true);
-            }
-        });
-
-    }
-
-    private void toggleHabLevel(int level, boolean left2) {
-        if (tempData.prematch_hab_level == level && (tempData.prematch_hab2_left == left2 || level != 2)) {
-            //selected already selected level, deselect
-            tempData.prematch_hab_level = 0;
-            tempData.prematch_hab2_left = false;
-        } else {
-            tempData.prematch_hab_level = level;
-            tempData.prematch_hab2_left = (level == 2 && left2);
-        }
-        loadData(tempData); //apply to UI
-    }
-
-    private void toggleCargoHatch(boolean hatch) {
-        if (hatch) {
-            tempData.prematch_robot_hatch = !tempData.prematch_robot_hatch;
-            tempData.prematch_robot_cargo = false;
-        } else {
-            tempData.prematch_robot_cargo = !tempData.prematch_robot_cargo;
-            tempData.prematch_robot_hatch = false;
-        }
-        loadData(tempData);
-    }
-
-    //private void switchSides() {
-    //    saveData(tempData);
-    //    tempData.scale_right = !tempData.scale_right;
-    //    tempData.near_switch_right = !tempData.near_switch_right;
-    //    tempData.far_switch_right = !tempData.far_switch_right;
-    //    Prefs.setRedLeft(getActivity(), !(Prefs.getRedLeft(getActivity(), true)));
-    //    loadData(tempData);
-    //}
+	private void switchSides() {
+		saveData(tempData);
+		Prefs.setRedLeft(getActivity(), !(Prefs.getRedLeft(getActivity(), true)));
+		//need to remap view when side changes
+		getGUIRefs(mainView);
+		setListeners();
+		loadData(tempData);
+	}
 
 }
