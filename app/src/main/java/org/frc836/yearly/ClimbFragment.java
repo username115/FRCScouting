1a4edd7d5da99d33ea417ddaf3973e25c7334cbd
username115 <username115@users.noreply.github.com>
2022-03-04 15:47:43 -0500

    Connect all of the UI elements in the Climb Fragment

--- app/src/main/java/org/frc836/yearly/ClimbFragment.java
+++ app/src/main/java/org/frc836/yearly/ClimbFragment.java
@@ -15,17 +15,22 @@
  */
 package org.frc836.yearly;
 
-import android.app.Activity;
 import android.os.Bundle;
+import android.os.Handler;
+import android.text.Editable;
+import android.text.TextWatcher;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageButton;
+import android.widget.RadioGroup;
 
 import androidx.annotation.NonNull;
 
 import org.frc836.database.MatchStatsStruct;
 import org.growingstems.scouting.MatchFragment;
-import org.growingstems.scouting.Prefs;
 import org.growingstems.scouting.R;
 
 
@@ -36,8 +41,34 @@ public class ClimbFragment extends MatchFragment {
 
 	private MatchStatsStruct tempData = new MatchStatsStruct();
 
+	private RadioGroup hangGroup;
+
+	private ImageButton imgLow;
+	private ImageButton imgMid;
+	private ImageButton imgHigh;
+	private ImageButton imgTraverse;
+
+	private EditText hangTime;
+
+	private Button timerStartStop;
+	private Button timerReset;
+
+	private final Handler timer = new Handler();
+	private boolean timerRunning = false;
+
+	private int time = 0;
 
 	private View mainView;
+
+	private final Runnable mUpdateTimeTask = new Runnable() {
+		@Override
+		public void run() {
+			if (timerRunning)
+				timer.postDelayed(mUpdateTimeTask, 1000);
+			time++;
+			hangTime.setText(String.valueOf(time));
+		}
+	};
 
 	public ClimbFragment() {
 		// Required empty public constructor
@@ -80,10 +111,15 @@ public class ClimbFragment extends MatchFragment {
 		getGUIRefs(mainView);
 		setListeners();
 		loadData(tempData);
+		timerStartStop.setText(R.string.start);
+		timer.removeCallbacks(mUpdateTimeTask);
+		timerRunning = false;
 	}
 
 	public void onPause() {
 		super.onPause();
+		timer.removeCallbacks(mUpdateTimeTask);
+		timerRunning = false;
 		saveData(tempData);
 	}
 
@@ -91,7 +127,14 @@ public class ClimbFragment extends MatchFragment {
 	public void saveData(MatchStatsStruct data) {
 		if (getView() == null || data == null || !displayed)
 			return;
-		//TODO save data for climb
+
+		data.hang_level = getHangLevelFromResId(hangGroup.getCheckedRadioButtonId());
+		data.hang_attempt = data.hang_level > 0
+				|| hangGroup.getCheckedRadioButtonId() == R.id.hangAttempt;
+
+		String temp = hangTime.getText().toString();
+		if (temp.length() > 0)
+			data.time_to_hang_s = Integer.parseInt(temp);
 	}
 
 	@Override
@@ -99,40 +142,142 @@ public class ClimbFragment extends MatchFragment {
 		tempData = data;
 		if (getView() == null || data == null || !displayed)
 			return;
-		// which side are we using
-		boolean redLeft = Prefs.getRedLeft(getActivity(), true);
 
-		Activity act = getActivity();
-		String pos;
-		if (act instanceof MatchActivity)
-			pos = ((MatchActivity) act).getPosition();
-		else
-			pos = Prefs.getPosition(getActivity(), "Red 1");
 
-		boolean blue = pos.contains("Blue");
+		switch (data.hang_level) {
+			case 1:
+				hangGroup.check(R.id.hangLow);
+				break;
+			case 2:
+				hangGroup.check(R.id.hangMid);
+				break;
+			case 3:
+				hangGroup.check(R.id.hangHigh);
+				break;
+			case 4:
+				hangGroup.check(R.id.hangTraverse);
+				break;
+			case 0:
+			default:
+				if (data.hang_attempt)
+					hangGroup.check(R.id.hangAttempt);
+				else
+					hangGroup.check(R.id.hangNone);
+				break;
+		}
 
-		//TODO load data
+		setHangLevelImg(data.hang_level);
+
+		if (data.time_to_hang_s > 0) {
+			hangTime.setText(String.valueOf(data.time_to_hang_s));
+			timerReset.setEnabled(true);
+			time = data.time_to_hang_s;
+		}
+		else {
+			hangTime.setText("");
+			timerReset.setEnabled(false);
+			time = 0;
+		}
 
 	}
 
 	private void getGUIRefs(View view) {
-		// which side are we using
-		boolean redLeft = Prefs.getRedLeft(getActivity(), true);
-		//and which position are we watching
-		Activity act = getActivity();
-		String pos;
-		if (act instanceof MatchActivity)
-			pos = ((MatchActivity) act).getPosition();
-		else
-			pos = Prefs.getPosition(getActivity(), "Red 1");
 
-		boolean blue = pos.contains("Blue");
+		hangGroup = view.findViewById(R.id.hangRadioGroup);
 
-		//TODO get references to GUI items
+		imgLow = view.findViewById(R.id.imageButtonLow);
+		imgMid = view.findViewById(R.id.imageButtonMiddle);
+		imgHigh = view.findViewById(R.id.imageButtonHigh);
+		imgTraverse = view.findViewById(R.id.imageButtonTraverse);
+
+		hangTime = view.findViewById(R.id.hangSeconds);
+
+		timerStartStop = view.findViewById(R.id.triggerHangTimerStartStop);
+		timerReset = view.findViewById(R.id.triggerHangTimerReset);
 	}
 
 	private void setListeners() {
+
+		hangGroup.setOnCheckedChangeListener((group, checkedId) -> {
+			int level = getHangLevelFromResId(checkedId);
+			setHangLevelImg(level);
+		});
+
+		hangTime.addTextChangedListener(new TextWatcher() {
+			@Override
+			public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+				//do nothing
+			}
+
+			@Override
+			public void onTextChanged(CharSequence s, int start, int before, int count) {
+				//do nothing
+			}
+
+			@Override
+			public void afterTextChanged(Editable s) {
+				timerReset.setEnabled(s.length() > 0);
+			}
+		});
+
+		timerReset.setOnClickListener(v -> {
+			hangTime.setText("");
+			time = 0;
+		});
+
+		timerStartStop.setOnClickListener(v -> {
+			if (timerRunning) {
+				timer.removeCallbacks(mUpdateTimeTask);
+				timerRunning = false;
+				timerStartStop.setText(R.string.start);
+			} else {
+				timerRunning = true;
+				timer.postDelayed(mUpdateTimeTask, 1000);
+				timerStartStop.setText(R.string.stop);
+			}
+		});
+
 		//TODO set up listeners
+	}
+
+	private int getHangLevelFromResId(int resId) {
+		switch (resId) {
+			case R.id.hangLow:
+				return 1;
+			case R.id.hangMid:
+				return 2;
+			case R.id.hangHigh:
+				return 3;
+			case R.id.hangTraverse:
+				return 4;
+			case R.id.hangNone:
+			case R.id.hangAttempt:
+			default:
+				return 0;
+		}
+	}
+
+	private void setHangLevelImg(int level) {
+		if (level == 1)
+			imgLow.setImageResource(R.drawable.fieldimage_hanger_side_low_green);
+		else
+			imgLow.setImageResource(R.drawable.fieldimage_hanger_side_low_red);
+
+		if (level == 2)
+			imgMid.setImageResource(R.drawable.fieldimage_hanger_side_middle_green);
+		else
+			imgMid.setImageResource(R.drawable.fieldimage_hanger_side_middle_red);
+
+		if (level == 3)
+			imgHigh.setImageResource(R.drawable.fieldimage_hanger_side_high_green);
+		else
+			imgHigh.setImageResource(R.drawable.fieldimage_hanger_side_high_red);
+
+		if (level >= 4)
+			imgTraverse.setImageResource(R.drawable.fieldimage_hanger_side_traverse_green);
+		else
+			imgTraverse.setImageResource(R.drawable.fieldimage_hanger_side_traverse_red);
+
 	}
 
 
